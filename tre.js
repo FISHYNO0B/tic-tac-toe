03637d7be7be9432342f3340c7353623feabac20
Fredrik Müntzing <101095169+Shawbooo@users.noreply.github.com>
2023-03-31 09:56:15 +0200

    Formatting (#1)
    * Formatting
    
    * Formatting

--- tre.js
+++ tre.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-   let numberOfClicks = 0;
-  
-   let reStart = document.getElementById("reStart")
-   
+
+    let numberOfClicks = 0;
+
+    let reStart = document.getElementById("reStart")
+
     const boxes = document.getElementsByClassName("box");
 
-    
     for (let i = 0; i < boxes.length; i++) {
-        
-        boxes[i].addEventListener("click", function() {
+
+        boxes[i].addEventListener("click", function () {
 
             if (this.classList.contains('not-clickeble')) {
                 return;
@@ -18,29 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
             if (numberOfClicks > 9) {
                 //numberOfClicks = 0;
                 // todo: reset color
-                    return;
+                return;
             }
             let clicks = document.getElementById("clicks")
             clicks.innerHTML = numberOfClicks;
 
             //todo every odd click should be another color.
-        
-            
+
+
             console.log(numberOfClicks % 2);
-            if (numberOfClicks % 2){
+            if (numberOfClicks % 2) {
                 this.style = 'background-color: green';
             } else {
                 this.style = 'background-color: red';
             }
-            
+
         });
     }
 
 
-   
-    reStart.addEventListener("click", function() {
+    reStart.addEventListener("click", function () {
         numberOfClicks = 0;
-  
+
 
         clicks.innerHTML = numberOfClicks;
         let boxes = document.getElementsByClassName("box");
@@ -51,5 +50,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     });
-})   
+})
 
