1ff4bd5064fbba6075bffdb2ce7d9a668fe1c8c9
Fredrik Muntzing <fredrik.muntzing@firstvet.com>
2023-03-31 10:14:29 +0200

    winner winner-feature

--- tre.js
+++ tre.js
@@ -2,6 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let numberOfClicks = 0;
 
+    let combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    function checkForWinner(boxes){
+        for (let comb of combs) {
+            if (
+                boxes[comb[0]].textContent == boxes[comb[1]].textContent &&
+                boxes[comb[1]].textContent == boxes[comb[2]].textContent &&
+                boxes[comb[0]].textContent != ''
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     let reStart = document.getElementById("reStart")
 
     const boxes = document.getElementsByClassName("box");
@@ -14,6 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
             if (this.classList.contains('not-clickeble')) {
                 return;
             }
+
+
             this.classList.add('not-clickeble');
             numberOfClicks += 1;
             if (numberOfClicks > 9) {
@@ -26,12 +52,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
             //todo every odd click should be another color.
 
-
-            console.log(numberOfClicks % 2);
             if (numberOfClicks % 2) {
+                this.classList.add('x')
+                this.textContent = 'x';
                 this.style = 'background-color: green';
             } else {
+                this.classList.add('o')
+                this.textContent = 'o';
                 this.style = 'background-color: red';
+            }
+
+            if(checkForWinner(boxes)){
+               document.getElementById('winner-container').innerHTML = `<h1>Winner is ${this.textContent}</h1>`
             }
 
         });
@@ -47,7 +79,8 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < boxes.length; i++) {
             boxes[i].style.backgroundColor = "chartreuse";
             boxes[i].classList.remove("not-clickeble");
-
+            boxes[i].classList.remove("x");
+            boxes[i].classList.remove("o");
         }
 
     });
