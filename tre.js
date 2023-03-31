39fa731ee02ffd4370bbe865ebe0660e1db2ff22
Fredrik Muntzing <fredrik.muntzing@firstvet.com>
2023-03-31 09:53:14 +0200

    Formatting

--- tre.js
+++ tre.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const boxes = document.getElementsByClassName("box");
 
-
     for (let i = 0; i < boxes.length; i++) {
 
         boxes[i].addEventListener("click", function () {
